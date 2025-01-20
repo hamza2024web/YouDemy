7791fb49d83a7c678bf15226babd7215990a51e9
hamza2024web <hamzaakroubi1528@gmail.com>
2025-01-20 11:17:21 +0100

    add the part of session destroy and log out button for users

--- project/src/Views/Admin/dashboard.php
+++ project/src/Views/Admin/dashboard.php
@@ -20,6 +20,7 @@
                     <li><a href="./UsersList.php" class="flex items-center hover:bg-blue-500 p-3 rounded transition duration-300"><i class="fas fa-chart-line"></i><span class="ml-2">Users List</span></a></li>
                     <li><a href="./cours.php" class="flex items-center bg-blue-700 p-3 rounded transition duration-300"><i class="fas fa-chart-line"></i><span class="ml-2">Enseignant Cours</span></a></li>
                     <li><a href="./statistique.php" class="flex items-center hover:bg-blue-500 p-3 rounded transition duration-300"><i class="fas fa-chart-line"></i><span class="ml-2">Statistique</span></a></li>
+                    <li><a href="../auth/logout.php" class="flex items-center hover:bg-blue-500 p-3 rounded transition duration-300"><i class="fas fa-chart-line"></i><span class="ml-2">Déconnexione</span></a></li>
                 </ul>
             </nav>
         </aside>
