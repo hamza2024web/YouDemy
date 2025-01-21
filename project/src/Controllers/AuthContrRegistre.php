b78a72a0d17c89a4766bced5cbcf3c07369cc5ef
hamza2024web <hamzaakroubi1528@gmail.com>
2025-01-21 10:21:56 +0100

    redirect user to login after he is create account

--- project/src/Controllers/AuthContrRegistre.php
+++ project/src/Controllers/AuthContrRegistre.php
@@ -7,24 +7,25 @@ class AuthContrRegistre {
     public function signup ($username , $email , $password , $role , $diplomat = null ,$status = null){
         $etudiantAndEnseignant = new EtudiantAndEnseignant();
         $utilisateur = $etudiantAndEnseignant->setEtudiantAndenseignant($username , $email ,$password,$role ,$diplomat,$status);
-
+        $pathUrl = "/src/views/";
         if($utilisateur == null){
             echo "please fields inputs and create your account ...";
         } else {
             if ($utilisateur->getStatus()=="Activation"){
-                $pathUrl = "/src/views/";
                 if($utilisateur->getRole()=="administrateur"){
-                    header("location:" . $pathUrl . "Admin/dashboard.php");
+                    header("location:" . $pathUrl . "auth/login.php");
                 }
                 else if($utilisateur->getRole()=="etudiant"){
-                    header("location:". $pathUrl . "etudiant/home.php");
+                    header("location:" . $pathUrl . "auth/login.php");
                 }
                 else if($utilisateur->getRole()=="enseignant"){
-                    header("location:". $pathUrl ."enseignant/home.php");
+                    header("location:" . $pathUrl . "auth/login.php");
                 }
             } elseif ($utilisateur->getStatus()=="suspension"){
+                header("location:" . $pathUrl . "auth/login.php");
                 echo "Votre compte a été suspenser";
             } elseif ($utilisateur->getStatus()=="Not Active"){
+                header("location:" . $pathUrl . "auth/login.php");
                 echo "Votre compte a été Disactiver";
             }
         }
